355462725e25c3710f5f64871e4497b33e98e0d5
Shmulik <29735690+Shmulik-Kravitz@users.noreply.github.com>
2021-04-14 12:43:42 +0300

    update dependencies, tsconfig.json improvements

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,12 @@ server.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
   reply.send(`Hello from ${request.url}`);
 });
 
-server.listen(80, "0.0.0.0", (err, address) => {
+const publicAddress = "0.0.0.0";
+
+server.listen(80, publicAddress, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
+  console.log(`Server listening at ${address.replace(publicAddress, "localhost")}`);
 });
