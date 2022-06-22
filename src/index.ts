d283e0632ee14c3f9e5ea9170751b5d172ac32d8
Shmulik <29735690+Shmulik-Kravitz@users.noreply.github.com>
2022-06-22 23:17:34 +0300

    upgrade dependencies to latest

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,21 @@ server.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
   reply.send(`Hello from ${request.url}`);
 });
 
-const publicAddress = "0.0.0.0";
+const start = async () => {
+  try {
+    const publicAddress = "0.0.0.0";
+    const port = 80;
 
-server.listen(80, publicAddress, (err, address) => {
-  if (err) {
-    console.error(err);
+    await server.listen({
+      port,
+      host: publicAddress,
+    });
+
+
+    console.log(`Server listening at http://localhost:${port}`);
+  } catch (err) {
+    server.log.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at ${address.replace(publicAddress, "localhost")}`);
-});
+};
+start();
