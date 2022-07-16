12ca69f15f70eabc22cc3738f82d79807016737b
Shmulik <29735690+Shmulik-Kravitz@users.noreply.github.com>
2022-07-17 00:34:58 +0300

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
 import fastify from "fastify";
 import { FastifyRequest, FastifyReply } from "fastify";
 
-const server = fastify();
-
-server.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
-  reply.send(`Hello from ${request.url}`);
-});
-
 const start = async () => {
   try {
     const publicAddress = "0.0.0.0";
-    const port = 80;
+    const port = 3000;
 
+    const server = fastify();
+
+    server.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
+      reply.send(`Hello from ${request.url}`);
+    });
     await server.listen({
       port,
       host: publicAddress,
     });
 
-
     console.log(`Server listening at http://localhost:${port}`);
   } catch (err) {
-    server.log.error(err);
+    console.error(err);
     process.exit(1);
   }
 };
